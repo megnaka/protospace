4a1e4d4dd4f1575462dc796184c0c084dc4f5154
imomochiii <orange.piano88@gmail.com>
2022-04-28 00:26:00 +0900

    index.html.erbの修正、ルートの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to:"users#index"
-  resources :prototypes, only:[:new, :create]
+  root to:"prototypes#index"
+  resources :prototypes, only:[:new, :create, :index]
 end
